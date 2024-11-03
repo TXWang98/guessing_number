e3e72aa473a2749edf67e17fc142b0ce8a1eaa8f
txwang <wtx19980928@pku.edu.cn>
2024-11-03 16:01:20 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const appDiv = document.getElementById('app');
+    appDiv.innerHTML = '<p>Hello, this is a shared JavaScript demo!</p>';
+});
