b84893ec5961144368828e362f5a8c530d612f79
TXWang98 <135817736+TXWang98@users.noreply.github.com>
2024-11-03 19:53:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -64,9 +64,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 const half_correct = cnt_correct - cnt_complete_correct;
                 feedback = `"${cnt_complete_correct} correct in both number and position, ${half_correct} only in number"`;
             }
-            if (this.guessright == false){
-                this.displayFeedback(feedback);
-            }
+            this.displayFeedback(feedback);
+            
         }
 
         displayFeedback(feedback) {
@@ -78,7 +77,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
             `;
             guessing_interaction.appendChild(newDiv.firstElementChild);
-            this.createInputField();
+            if (this.guessright == true){
+                let currentUrl = window.location.href;
+                window.location.href = newUrl;
+            }else{
+                this.createInputField();
+            }
         }
 
         createInputField() {
